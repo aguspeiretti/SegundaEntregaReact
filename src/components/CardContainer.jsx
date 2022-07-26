8bf3b4f9826dfaea276c93297a152b20f2926b23
aguspeiretti <aguspeiretti@gmail.com>
2022-07-26 18:36:17 -0300

    26/7

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,80 +2,21 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import "../components/CardContainer.css"
 import CardContainerPrendas1 from './CardContainerPrendas1'
+import productos from  "../productos.json"
 
 const CardContainer = () => {
 
 const [items, setItems] = useState([])
 
 useEffect(() => {
-    setItems([
-      {
-        id: 1,
-        titulo: 'Producto 1',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      },
-      {
-        id: 2,
-        titulo: 'Producto 2',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      },
-      {
-        id: 3,
-        titulo: 'Producto 3',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      },
-  
-      {
-        id: 4,
-        titulo: 'Producto 4',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      },
-      {
-        id: 5,
-        titulo: 'Producto 5',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      },
-      {
-        id:6,
-        titulo: 'Producto 6',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      },
-      {
-        id: 7,
-        titulo: 'Producto 7',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      },
-      {
-        id: 8,
-        titulo: 'Producto 8',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      },
-      {
-        id: 9,
-        titulo: 'Producto 9',
-        stock: 10,
-        inicial: 1,
-        img: ''
-      }
 
-    ])
+  setTimeout(() => {
+    
+    setItems(productos)
+  }, 3000);
+    
 },[])
+
 
 
 
